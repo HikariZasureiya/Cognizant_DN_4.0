e08593ef78bd4424490368b58d6a1290049725ff
HikariZasureiya <info.d.podder@gmail.com>
2025-08-10 01:44:41 +0530

    c1

---
+++ week8/handson1.sh
@@ -0,0 +1,11 @@
+# show git version
+git version
+
+# user level configuration
+
+git config --global user.name "HikariZasureiya"
+git config --global user.email "info.d.podder@gmail.com"
+
+#check global list
+git config --global --list
+
